b2792475dcfde8c3bf06643d1f5f95900d9d18dd
cerihughes <hughesceri+github@gmail.com>
2023-07-26 14:30:35 +0100

    Add initial project structure

---
+++ FCSDKSample/FCSDKSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FCSDKSampleApp.swift
+//  FCSDKSample
+//
+//  Created by Awen CS on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct FCSDKSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
