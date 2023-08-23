79b719423bd70d825a9211893008a8f8ec594a4f
cerihughes <hughesceri+github@gmail.com>
2023-08-23 15:54:40 +0100

    End calls logic

--- FCSDKSample/UI/Call/CallView.swift
+++ FCSDKSample/UI/Call/CallView.swift
@@ -9,10 +9,16 @@ import SnapKit
 import UIKit
 
 class CallView: UIView {
+    enum State {
+        case uninitialised, initialising, initialised, calling, callInProgress, ending
+    }
+
     let localVideoView = UIView()
     let remoteVideoView = UIView()
-    let callButton = UIButton.createCallButton()
-    
+    let initialiseButton = UIButton.create(title: "Initialise")
+    let callButton = UIButton.create(title: "Make Call")
+    let endButton = UIButton.create(title: "End Call")
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         commonInit()
@@ -29,7 +35,7 @@ class CallView: UIView {
         remoteVideoView.backgroundColor = .lightGray
         localVideoView.backgroundColor = .gray
 
-        addSubviews(remoteVideoView, localVideoView, callButton)
+        addSubviews(remoteVideoView, localVideoView, initialiseButton, callButton, endButton)
 
         remoteVideoView.snp.makeConstraints { make in
             make.edges.equalTo(safeAreaLayoutGuide)
@@ -39,10 +45,29 @@ class CallView: UIView {
             make.size.equalTo(remoteVideoView).dividedBy(4)
             make.trailing.bottom.equalTo(remoteVideoView).inset(24)
         }
-        
-        callButton.snp.makeConstraints { make in
+
+        initialiseButton.snp.makeConstraints { make in
             make.trailing.equalTo(localVideoView)
             make.top.equalTo(remoteVideoView).inset(24)
+        }
+
+        callButton.snp.makeConstraints { make in
+            make.top.trailing.equalTo(initialiseButton)
+        }
+
+        endButton.snp.makeConstraints { make in
+            make.top.trailing.equalTo(callButton)
+        }
+    }
+
+    var state: State = .uninitialised {
+        didSet {
+            initialiseButton.isHidden = state.isNotIn(.uninitialised, .initialising)
+            initialiseButton.isEnabled = state != .initialising
+            callButton.isHidden = state.isNotIn(.initialised, .calling)
+            callButton.isEnabled = state != .calling
+            endButton.isHidden = state.isNotIn(.callInProgress, .ending)
+            endButton.isEnabled = state != .ending
         }
     }
 }
@@ -54,5 +79,15 @@ private extension UIButton {
         button.setTitle("Make Call", for: .normal)
         button.setTitleColor(.black, for: .normal)
         return button
+    }
+}
+
+private extension CallView.State {
+    func isIn(_ states: CallView.State...) -> Bool {
+        states.contains(self)
+    }
+
+    func isNotIn(_ states: CallView.State...) -> Bool {
+        !states.contains(self)
     }
 }
--- FCSDKSample/UI/Call/CallViewModel.swift
+++ FCSDKSample/UI/Call/CallViewModel.swift
@@ -7,10 +7,51 @@
 
 import UIKit
 
+protocol CallViewModelDelegate: AnyObject {
+    func callViewModelDidEndCall(_ viewModel: CallViewModel)
+}
+
 class CallViewModel {
     private let callManager: CallManager
-    
+
+    weak var delegate: CallViewModelDelegate?
+
     init(callManager: CallManager) {
         self.callManager = callManager
+    }
+
+    var localVideoView: UIView? {
+        get {
+            callManager.localVideoView
+        }
+        set {
+            callManager.localVideoView = newValue
+        }
+    }
+    var remoteVideoView: UIView? {
+        get {
+            callManager.remoteVideoView
+        }
+        set {
+            callManager.remoteVideoView = newValue
+        }
+    }
+
+    func initialise() async -> Bool {
+        await callManager.initialise()
+    }
+
+    func makeCall() async -> Bool {
+        await callManager.makeCall()
+    }
+
+    func endCall() -> Bool {
+        callManager.endCall()
+    }
+}
+
+extension CallViewModel: CallManagerDelegate {
+    func callManagerDidEndCall(_ callManager: CallManager) {
+        delegate?.callViewModelDidEndCall(self)
     }
 }
